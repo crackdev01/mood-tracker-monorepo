627f320878f499e13a9696920cdf694b6d51943a
crackdev01 <blackbox0823@gmail.com>
2021-02-19 20:28:17 +0100

    setup: commitlinter and base rules

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'ci',
+        'docs',
+        'feat',
+        'fix',
+        'refactor',
+        'setup',
+        'test',
+      ],
+    ],
+  },
+};
