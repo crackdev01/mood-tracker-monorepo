d44a98ce8be4868089b6ba211791283f284331db
crackdev01 <blackbox0823@gmail.com>
2021-02-19 23:57:23 +0100

    feat: generate initial migration scripts and update readme and env values

---
+++ packages/backend/rest-mood-tracker/src/db/migrations/1613774993173-InitialDbCreation.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class InitialDbCreation1613774993173 implements MigrationInterface {
+    name = 'InitialDbCreation1613774993173'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "user" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "username" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id"))`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`);
+    }
+
+}
