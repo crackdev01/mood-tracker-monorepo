a5acf2866954243dd64c595ae97fc327c49ae0bb
crackdev01 <blackbox0823@gmail.com>
2021-02-20 18:36:00 +0100

    setup: mock css/scss imports through identity-obj-proxy

--- packages/frontend/ui-mood-tracker/src/App.tsx
+++ packages/frontend/ui-mood-tracker/src/App.tsx
@@ -3,6 +3,8 @@ import { BrowserRouter } from 'react-router-dom';
 
 import SiteHeader from './components/SiteHeader';
 
+import 'semantic-ui-css/semantic.min.css'
+
 function App() {
   return (
     <BrowserRouter>
--- packages/frontend/ui-mood-tracker/test/unit/jest-unit.js
+++ packages/frontend/ui-mood-tracker/test/unit/jest-unit.js
@@ -1,6 +1,6 @@
 module.exports = {
   moduleNameMapper: {
-    '\\.(css|scss)$': '../../src/__mocks__/styleMock.js'
+    "\\.(css|scss)$": 'identity-obj-proxy',
   },
   globals: {
     'ts-jest': {
