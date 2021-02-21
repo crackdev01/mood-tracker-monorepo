a3a7291d2691fda2a341d306d493f6c3abad33b0
crackdev01 <blackbox0823@gmail.com>
2021-02-21 19:43:34 +0100

    feat: dynamically update app locale

--- packages/backend/rest-mood-tracker/src/mood/mood.controller.ts
+++ packages/backend/rest-mood-tracker/src/mood/mood.controller.ts
@@ -9,26 +9,31 @@ import {
 } from '@nestjs/common';
 
 import { MoodService } from 'src/mood/mood.service';
+import { Public } from 'src/auth/auth.decorator';
 
 @Controller('mood')
 export class MoodController {
   constructor(private readonly moodService: MoodService) {}
 
+  @Public()
   @Get('entries')
   async getMoodEntries(@Request() req) {
     return await this.moodService.findMoodEntriesByUser(req.user);
   }
 
+  @Public()
   @Post('entry')
   async postMoodEntry(@Request() req, @Body() payload) {
     return await this.moodService.insertMoodEntry(payload);
   }
 
+  @Public()
   @Patch('entry')
   async patchMoodEntry(@Request() req, @Body() payload) {
     return await this.moodService.updateMoodEntry(payload);
   }
 
+  @Public()
   @Delete('entry')
   async deleteMoodEntry(@Request() req, @Body() payload) {
     return await this.moodService.deleteMoodEntry(payload);
--- packages/frontend/ui-mood-tracker/src/components/SiteHeader.tsx
+++ packages/frontend/ui-mood-tracker/src/components/SiteHeader.tsx
@@ -3,14 +3,18 @@ import { useTranslation } from 'react-i18next';
 import { Link, useLocation } from 'react-router-dom';
 import { Header, Menu } from 'semantic-ui-react';
 
-import '../App.scss';
+import '../scss/site-header.scss';
 
 const SiteHeader = () => {
-  const { t } = useTranslation(['SiteHeader']);
+  const { t, i18n } = useTranslation(['SiteHeader']);
   const location = useLocation();
 
+  const updateLanguage = async (locale: 'en' | 'de') => {
+    await i18n.changeLanguage(locale);
+  };
+
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary className="site-header">
       <Menu.Item header>
         <Header as="h3">
           <Link to="login">{t('header')}</Link>
@@ -21,6 +25,20 @@ const SiteHeader = () => {
       </Menu.Item>
       <Menu.Item active={location.pathname === '/statistics'}>
         <Link to="/statistics">{t('statistics')}</Link>
+      </Menu.Item>
+      <Menu.Item position="right" className="locale">
+        <span
+          className={i18n.language === 'en' ? 'locale--active' : 'locale--inactive'}
+          onClick={() => updateLanguage('en')}
+        >
+          EN
+        </span>
+        <span
+          className={i18n.language === 'de' ? 'locale--active' : 'locale--inactive'}
+          onClick={() => updateLanguage('de')}
+        >
+          DE
+        </span>
       </Menu.Item>
     </Menu>
   );
