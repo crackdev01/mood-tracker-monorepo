f71d5891ab6691d67eaae14b703125d54dd355cc
crackdev01 <blackbox0823@gmail.com>
2021-02-22 20:29:11 +0100

    feat: add end points for mood api

--- packages/frontend/ui-mood-tracker/src/api/mood.ts
+++ packages/frontend/ui-mood-tracker/src/api/mood.ts
@@ -3,3 +3,15 @@ import { moodTrackerApi } from './core';
 export async function getMoodEntriesApi(): Promise<any> {
   return await moodTrackerApi.get('/mood/entries');
 }
+
+export async function postMoodEntry(payload: any): Promise<void> {
+  return await moodTrackerApi.post('/mood/entry', payload);
+}
+
+export async function editMoodEntry(payload: any): Promise<void> {
+  return await moodTrackerApi.put('/mood/entry', payload);
+}
+
+export async function deleteMoodEntry(payload: any): Promise<void> {
+  return await moodTrackerApi.delete('/mood/entry', payload);
+}
