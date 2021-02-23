441682529fd964d0e359acc62e3d6c7c795188a9
crackdev01 <blackbox0823@gmail.com>
2021-02-23 09:43:34 +0100

    fix: linter errors on the backend

--- packages/backend/rest-mood-tracker/src/auth/jwt.strategy.ts
+++ packages/backend/rest-mood-tracker/src/auth/jwt.strategy.ts
@@ -3,7 +3,6 @@ import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 
 import { jwtConstants } from './constants';
-import { UserEntity } from 'src/db/entities/user.entity';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
--- packages/backend/rest-mood-tracker/src/mood/mood.service.ts
+++ packages/backend/rest-mood-tracker/src/mood/mood.service.ts
@@ -1,4 +1,4 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { MoodRepository } from 'src/db/repositories/mood.repository';
 import { MoodEntity } from 'src/db/entities/mood.entity';
