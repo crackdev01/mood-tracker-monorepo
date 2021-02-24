87640b507b6390571ec1aa968eb660b6f65f1f4b
crackdev01 <blackbox0823@gmail.com>
2021-02-24 15:21:45 +0100

    fix: add missing tests and update coverage

--- packages/frontend/ui-mood-tracker/src/__tests__/unit/store/user/reducers.spec.ts
+++ packages/frontend/ui-mood-tracker/src/__tests__/unit/store/user/reducers.spec.ts
@@ -21,6 +21,46 @@ describe('userReducer', () => {
     });
   });
 
+  test(UserActions.SET_LOCATION_SUCCESS, () => {
+    const action = {
+      type: UserActions.SET_LOCATION_SUCCESS,
+      data: 'mock-location',
+    };
+    expect(user(userState, action)).toEqual({
+      user: {
+        accessToken: '',
+        decodedAccessToken: null,
+        location: 'mock-location',
+      },
+    });
+  });
+
+  test(UserActions.SET_LOCATION_DENIED, () => {
+    const action = {
+      type: UserActions.SET_LOCATION_DENIED,
+    };
+    expect(user(userState, action)).toEqual({
+      user: {
+        accessToken: '',
+        decodedAccessToken: null,
+        location: UserActions.SET_LOCATION_DENIED,
+      },
+    });
+  });
+
+  test(UserActions.SET_LOCATION_ERROR, () => {
+    const action = {
+      type: UserActions.SET_LOCATION_ERROR,
+    };
+    expect(user(userState, action)).toEqual({
+      user: {
+        accessToken: '',
+        decodedAccessToken: null,
+        location: UserActions.SET_LOCATION_ERROR,
+      },
+    });
+  });
+
   test(UserActions.LOGOUT_USER, () => {
     const action = {
       type: UserActions.LOGOUT_USER,
--- packages/frontend/ui-mood-tracker/src/__tests__/unit/store/user/saga.spec.ts
+++ packages/frontend/ui-mood-tracker/src/__tests__/unit/store/user/saga.spec.ts
@@ -1,6 +1,15 @@
 import { expectSaga, testSaga } from 'redux-saga-test-plan';
 
-import { loginUser, loginUserCall, logoutUser, logoutUserCall } from '../../../../store/user/saga';
+import {
+  loginUser,
+  loginUserCall,
+  logoutUser,
+  logoutUserCall, setUserLocationDenied, setUserLocationDeniedCall,
+  setUserLocationError,
+  setUserLocationErrorCall,
+  setUserLocationSuccess,
+  setUserLocationSuccessCall,
+} from '../../../../store/user/saga';
 import { authenticateUser } from '../../../../api/user';
 import { mockUser } from '../../../fixtures';
 import { UserActions } from '../../../../store/user/types';
@@ -10,6 +19,30 @@ describe('userSaga', () => {
   describe('action mappers', () => {
     test('loginUser', () => {
       testSaga(loginUser).next().takeEvery(UserActions.FETCH_USER, loginUserCall).finish().isDone();
+    });
+
+    test('setUserLocationSuccess', () => {
+      testSaga(setUserLocationSuccess)
+        .next()
+        .takeEvery(UserActions.SET_LOCATION_SUCCESS, setUserLocationSuccessCall)
+        .finish()
+        .isDone();
+    });
+
+    test('setUserLocationError', () => {
+      testSaga(setUserLocationError)
+        .next()
+        .takeEvery(UserActions.SET_LOCATION_ERROR, setUserLocationErrorCall)
+        .finish()
+        .isDone();
+    });
+
+    test('setUserLocationDenied', () => {
+      testSaga(setUserLocationDenied)
+        .next()
+        .takeEvery(UserActions.SET_LOCATION_DENIED, setUserLocationDeniedCall)
+        .finish()
+        .isDone();
     });
 
     test('logoutUser', () => {
@@ -38,6 +71,24 @@ describe('userSaga', () => {
           },
         })
         .put({ type: UserActions.RENDER_USER, data: mockUser })
+        .run();
+    });
+
+    test('setUserLocationSuccessCall', () => {
+      return expectSaga(setUserLocationSuccessCall, { payload: { location: 'mock-location' } })
+        .put({ type: UserActions.RENDER_USER, data: 'mock-location' })
+        .run();
+    });
+
+    test('setUserLocationErrorCall', () => {
+      return expectSaga(setUserLocationErrorCall)
+        .put({ type: UserActions.RENDER_USER, data: UserActions.SET_LOCATION_ERROR })
+        .run();
+    });
+
+    test('setUserLocationDeniedCall', () => {
+      return expectSaga(setUserLocationDeniedCall)
+        .put({ type: UserActions.RENDER_USER, data: UserActions.SET_LOCATION_DENIED })
         .run();
     });
 
