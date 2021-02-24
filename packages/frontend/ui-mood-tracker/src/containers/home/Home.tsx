2a8e42d932514db7bbd3244efcf1dbd330866453
crackdev01 <blackbox0823@gmail.com>
2021-02-24 10:36:42 +0100

    feat: move fetching of mood entries outside of Home

--- packages/frontend/ui-mood-tracker/src/containers/home/Home.tsx
+++ packages/frontend/ui-mood-tracker/src/containers/home/Home.tsx
@@ -1,50 +1,24 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
 
 import ProtectedRoute from '../../components/ProtectedRoute';
 import Login from '../login/Login';
 import MoodEntry from '../mood-entry/MoodEntry';
 import Statistics from '../statistics/Statistics';
 import SiteHeader from '../../components/site-header/SiteHeader';
-import LocationModal from '../../components/location/LocationModal';
-import { MoodActions } from '../../store/mood/types';
 import { ApplicationState } from '../../store';
 
 import './home.scss';
 
 const Home = () => {
-  const dispatch = useDispatch();
-  const [coordinates, setCoordinates] = useState({ lat: 0, long: 0 });
   const user = useSelector((state: ApplicationState) => state.userReducer.user);
 
-  const loadLocation = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition((position) =>
-        setCoordinates({
-          lat: position.coords.latitude,
-          long: position.coords.longitude,
-        })
-      );
-    }
-  };
-
   const isAuthenticated = !!user.accessToken;
-
-  useEffect(() => {
-    dispatch({
-      type: MoodActions.FETCH_MOODS,
-    });
-    // TODO: setup user module and dispatch action to fetch coordinates from there.
-    // loadLocation();
-  }, [user]);
 
   return (
     <BrowserRouter>
       <SiteHeader />
-
-      <LocationModal />
 
       <article className="app-content">
         <Switch>
--- packages/frontend/ui-mood-tracker/src/containers/mood-entry/MoodEntry.tsx
+++ packages/frontend/ui-mood-tracker/src/containers/mood-entry/MoodEntry.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Header } from 'semantic-ui-react';
 
@@ -6,9 +7,25 @@ import AddEntryModal from '../../components/mood-entry/add-entry/AddEntryModal';
 import MoodList from '../../components/mood-entry/mood-list/MoodList';
 
 import './mood-entry.scss';
+import { ApplicationState } from '../../store';
+import { MoodActions } from '../../store/mood/types';
 
 const MoodEntry = () => {
+  const dispatch = useDispatch();
   const { t } = useTranslation(['MoodEntry']);
+  const user = useSelector((state: ApplicationState) => state.userReducer.user);
+
+  const triggerFetchMoodEntries = () => {
+    if (user.accessToken) {
+      dispatch({
+        type: MoodActions.FETCH_MOODS,
+      });
+    }
+  };
+
+  useEffect(() => {
+    triggerFetchMoodEntries();
+  }, [user]);
 
   return (
     <article className="mood-entry">
